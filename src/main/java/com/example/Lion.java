f97694c167fda60a7aa0d2179acfdd77c4265cfd
Svetlana Demenok <svetlanka085@mail.ru>
2022-05-14 19:01:04 +0300

    Изолировала Lion от Feline

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -6,17 +6,19 @@ public class Lion {
 
     boolean hasMane;
 
-    public Lion(String sex) throws Exception {
+    Feline feline;
+
+    public Lion(Feline feline, String sex) throws Exception {
+        this.feline = feline;
+
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
     }
-
-    Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
@@ -27,6 +29,6 @@ public class Lion {
     }
 
     public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
+        return feline.eatMeat();
     }
 }
